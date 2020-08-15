5fe9df21ac464010a738ff6cc5611395ce074530
ch <mkmsacc@outlook.com>
2020-08-15 23:29:40 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/main.css">
+
+    <title>Laravel</title>
+
+</head>
+
+<body>
+    <div class="flex border border-gray-300" id="app">
+
+        <h1>Hello</h1>
+        <h1>Hello</h1>
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
